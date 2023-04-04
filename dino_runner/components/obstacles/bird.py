f5900e3c73dbfeafda703e748da8ecc0287c99bd
MariToledo2960 <129456978+MariToledo2960@users.noreply.github.com>
2023-04-04 17:09:15 -0500

    Homework Third Class

---
+++ dino_runner/components/obstacles/bird.py
@@ -0,0 +1,23 @@
+import random
+from dino_runner.components.obstacles.obstacle import Obstacle
+from dino_runner.utils.constants import BIRD 
+
+
+class Bird(Obstacle):
+    def __init__(self, image):
+        self.type = 0
+        super().__init__(image, self.type)
+        self.rect.y = random.randint(50,300)
+        self.bird_index = 0
+        self.image = BIRD
+    
+    def fly(self, screen):
+        if self.bird_index > 9:
+            self.bird_index = 0
+        screen.blit(self.image[self.index // 5], self.rect)
+        self.bird_index += 1
+        
+        
+        
+        
+        
--- dino_runner/components/obstacles/obstacle.py
+++ dino_runner/components/obstacles/obstacle.py
@@ -4,9 +4,9 @@ from pygame.sprite import Sprite
 from dino_runner.utils.constants import SCREEN_WIDTH
 
 class Obstacle(Sprite):
-    def __init__(self, image, obtacle_type):
+    def __init__(self, image, obstacle_type):
         self.image = image
-        self.obstacle_type = obtacle_type
+        self.obstacle_type = obstacle_type
         self.rect = self.image[self.obstacle_type].get_rect()
         self.rect.x = SCREEN_WIDTH
 
